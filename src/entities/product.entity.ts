50c2de11d0de20983b473b5c61b4f9efad02f741
vahid-nejad <vahid884@gmail.com>
2022-04-30 21:21:03 +0430

    create brands

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,10 +1,34 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { type } from 'os';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Brand } from './brand.entity';
+import { ProductCategory } from './productCategory.entity';
+import { ProductColorVariant } from './productColorVariant.entity';
+import { ProductSpecific } from './productSpecific.entity';
+import { ProductVariant } from './productVariant.entity';
 
-@Entity({ name: 'Products' })
+@Entity()
 export class Product {
   @PrimaryGeneratedColumn()
-  id: number;
-
+  id?: number;
   @Column()
-  name: string;
+  name?: string;
+  @Column('json')
+  images?: string[];
+  @Column()
+  cardImage?: string;
+
+  @OneToMany(type=>ProductColorVariant, productColorVariant=>productColorVariant.product)
+  colorVariants?: ProductColorVariant[];
+
+  @OneToMany(type=>ProductVariant, productVariant=>productVariant.product)
+  variants?: ProductVariant[];
+
+@OneToMany(type=>ProductSpecific, productSpecific=>productSpecific.product)
+  specifics?: ProductSpecific[];
+
+  @ManyToOne(type=>ProductCategory, productCategory=>productCategory.products)
+  category?: ProductCategory;
+
+  @ManyToOne(type=>Brand, brand=>brand.products)
+  brand?: Brand;
 }
